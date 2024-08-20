6125cad2326d092abe5f42c176f7164663af4ce6
Tyrone Tudehope <tyronetudehope@gmail.com>
2024-08-20 10:12:10 +0200

    Use tabs for indentation

--- ecs.php
+++ ecs.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 use fostercommerce\ecs\ECSConfig;
 
 return ECSConfig::configure()->withPaths([
-    __DIR__ . '/src',
-    __FILE__,
+	__DIR__ . '/src',
+	__FILE__,
 ]);
--- src/ECSConfig.php
+++ src/ECSConfig.php
@@ -6,31 +6,35 @@ namespace fostercommerce\ecs;
 
 use PHP_CodeSniffer\Standards\Generic\Sniffs\CodeAnalysis\AssignmentInConditionSniff;
 use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
+use PhpCsFixer\Fixer\Whitespace\IndentationTypeFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig as BaseECSConfig;
 use Symplify\EasyCodingStandard\Configuration\ECSConfigBuilder;
+use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 class ECSConfig
 {
-    public static function configure(): ECSConfigBuilder
-    {
-        return BaseECSConfig::configure()
-            ->withParallel()
-            ->withSets([
-                SetList::ARRAY,
-                SetList::COMMENTS,
-                SetList::CONTROL_STRUCTURES,
-                SetList::DOCBLOCK,
-                SetList::NAMESPACES,
-                SetList::PHPUNIT,
-                SetList::SPACES,
-                SetList::STRICT,
-                SetList::CLEAN_CODE,
-                SetList::PSR_12,
-            ])
-            ->withSkip([
-                DeclareStrictTypesFixer::class,
-                AssignmentInConditionSniff::class,
-            ]);
-    }
+	public static function configure(): ECSConfigBuilder
+	{
+		return BaseECSConfig::configure()
+			->withParallel()
+			->withSets([
+				SetList::ARRAY,
+				SetList::COMMENTS,
+				SetList::CONTROL_STRUCTURES,
+				SetList::DOCBLOCK,
+				SetList::NAMESPACES,
+				SetList::PHPUNIT,
+				SetList::SPACES,
+				SetList::STRICT,
+				SetList::CLEAN_CODE,
+				SetList::PSR_12,
+			])
+			->withRules([IndentationTypeFixer::class])
+			->withSpacing(indentation: Option::INDENTATION_TAB)
+			->withSkip([
+				DeclareStrictTypesFixer::class,
+				AssignmentInConditionSniff::class,
+			]);
+	}
 }
